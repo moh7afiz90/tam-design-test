36755e2547a5f33c0f69065384fbd19209473d36
Mohanad Elhag <moh.7afiz@gmail.com>
2017-11-29 19:49:34 +0300

    Added project files

---
+++ js/index.js
@@ -0,0 +1,103 @@
+
+// Desig Section
+var task_1 = document.getElementById('task_1');
+function checked_1() {
+    if (task_1.className == 'check' ) {
+      task_1.className = '';
+    } else {
+      task_1.className = 'check';
+    }
+    return false;
+};
+var task_2 = document.getElementById('task_2');
+function checked_2() {
+    if (task_2.className == 'check' ) {
+      task_2.className = '';
+    } else {
+      task_2.className = 'check';
+    }
+};
+var task_3 = document.getElementById('task_3');
+function checked_3() {
+    if (task_3.className == 'check' ) {
+      task_3.className = '';
+    } else {
+      task_3.className = 'check';
+    }
+};
+var task_4 = document.getElementById('task_4');
+function checked_4() {
+    if (task_4.className == 'check' ) {
+      task_4.className = '';
+    } else {
+      task_4.className = 'check';
+    }
+};
+var task_5 = document.getElementById('task_5');
+function checked_5() {
+    if (task_5.className == 'check' ) {
+      task_5.className = '';
+    } else {
+      task_5.className = 'check';
+    }
+};
+// Improvement Section
+var task_6 = document.getElementById('task_6');
+function checked_6() {
+    if (task_6.className == 'check' ) {
+      task_6.className = '';
+    } else {
+      task_6.className = 'check';
+    }
+};
+var task_7 = document.getElementById('task_7');
+function checked_7() {
+    if (task_7.className == 'check' ) {
+      task_7.className = '';
+    } else {
+      task_7.className = 'check';
+    }
+};
+var task_8 = document.getElementById('task_8');
+function checked_8() {
+    if (task_8.className == 'check' ) {
+      task_8.className = '';
+    } else {
+      task_8.className = 'check';
+    }
+};
+var task_9 = document.getElementById('task_9');
+function checked_9() {
+    if (task_9.className == 'check' ) {
+      task_9.className = '';
+    } else {
+      task_9.className = 'check';
+    }
+};
+// Others Section
+var task_10 = document.getElementById('task_10');
+function checked_10() {
+    if (task_10.className == 'check' ) {
+      task_10.className = '';
+    } else {
+      task_10.className = 'check';
+    }
+};
+var task_11 = document.getElementById('task_11');
+function checked_11() {
+    if (task_11.className == 'check' ) {
+      task_11.className = '';
+    } else {
+      task_11.className = 'check';
+    }
+};
+// Sandwich Menu
+function openSlideMenu() {
+  document.getElementById('side-menu').style.width = '264px';
+  document.getElementById('main_area').style.marginRight = '264px';
+}
+
+function closeSlideMenu() {
+  document.getElementById('side-menu').style.width = '0';
+  document.getElementById('main_area').style.marginRight = '0';
+}
